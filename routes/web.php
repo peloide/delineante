59e3fc1a221929f8c799cfff85aaf579d39973df
Jorge Suarez <jorgesuarezlozano6@gmail.com>
2020-04-09 20:42:48 +0200

    Creación de modelo y prueba de inserts, deletes, updates, selects...

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Proyecto;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,6 +21,78 @@ Route::get('/inicio', 'PaginasController@inicio');
 Route::get('/proyectos', 'PaginasController@proyectos')->name('proyectos');
 Route::get('/contacto', 'PaginasController@contacto')->name('contacto');
 Route::get('/mostrar', 'PaginasController@mostrar');
+
+/*
+Route::get("leer", function(){
+
+    $proyectos = Proyecto::all();
+
+    foreach($proyectos as $proyecto){
+        echo '<p>';
+        echo 'nombre: ' .$proyecto->nombre_proyecto .' -> tipo: ' .$proyecto->tipo;
+        echo '</p>';
+    }
+});
+*/
+
+Route::get('leer', function () {
+
+    $proyectos = Proyecto::where('tipo', 'madera')->get();
+    foreach($proyectos as $proyecto){
+        /*echo '<p>';
+        echo 'nombre: ' .$proyecto->nombre_proyecto .' -> tipo: ' .$proyecto->tipo;
+        echo '</p>';
+    */
+    return $proyectos;
+    }
+
+    
+});
+
+Route::get('/insertar', function () {
+
+    $proyectos = new Proyecto;
+
+    $proyectos->nombre_proyecto="Casa de madera";
+    $proyectos->tipo="tipo";
+    $proyectos->descripcion="Casa de madera de roble con tejado de pizarra y remates en caoba";
+    $proyectos->imagen="casa1.jpg";
+    $proyectos->precio="17.500";
+    $proyectos->seccion="casas";
+
+    $proyectos->save();
+    
+});
+
+/*
+Route::get('/actualizar', function () {
+
+    $proyectos = Proyecto::find(7);
+
+    $proyectos->nombre_proyecto="Casa de madera";
+    $proyectos->tipo="tipo";
+    $proyectos->descripcion="Casa de madera de roble con tejado de pizarra y remates en caoba";
+    $proyectos->imagen="casa1.jpg";
+    $proyectos->precio="30000";
+    $proyectos->seccion="casas";
+
+    $proyectos->save();
+    
+});
+*/
+
+Route::get('/actualizar', function () {
+
+    Proyecto::where('seccion', 'carbon')->update(['seccion'=>'frutal']);
+    
+});
+
+Route::get('/borrar', function () {
+
+    Proyecto::where('seccion', 'casas')->delete();
+    
+});
+
 
 
 // Nos genera todas las rutas
